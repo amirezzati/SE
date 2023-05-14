89ab4c75127edd97e688554ff2dd3ac78fa0438a
Amir Ezzati <amezzati9@gmail.com>
2023-05-14 19:17:55 +0330

    add HumHub project

---
+++ Phase2 - Review Some Social Media Apps/humhub/protected/config/dynamic.php
@@ -0,0 +1,38 @@
+<?php return array (
+  'components' => 
+  array (
+    'db' => 
+    array (
+      'class' => 'yii\\db\\Connection',
+      'dsn' => 'mysql:host=localhost;dbname=humhub_database',
+      'username' => 'root',
+      'password' => '',
+    ),
+    'user' => 
+    array (
+    ),
+    'mailer' => 
+    array (
+      'transport' => 
+      array (
+        'dsn' => 'native://default',
+      ),
+    ),
+  ),
+  'params' => 
+  array (
+    'installer' => 
+    array (
+      'db' => 
+      array (
+        'installer_hostname' => 'localhost',
+        'installer_database' => 'humhub_database',
+      ),
+    ),
+    'config_created_at' => 1684078166,
+    'horImageScrollOnMobile' => NULL,
+    'databaseInstalled' => true,
+  ),
+  'name' => NULL,
+  'language' => 'en-US',
+); ?>
